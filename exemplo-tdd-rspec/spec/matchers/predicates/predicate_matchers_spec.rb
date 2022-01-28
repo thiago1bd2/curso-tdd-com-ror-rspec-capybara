5f1963ee3ba55099c6cd22a26c02a67b7fd37d2b
thiago1bd2 <thiagodaqui@gmail.com>
2022-01-28 14:03:39 -0300

    predicate test refactor

--- exemplo-tdd-rspec/spec/matchers/predicates/predicate_matchers_spec.rb
+++ exemplo-tdd-rspec/spec/matchers/predicates/predicate_matchers_spec.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-# require './spec/support/numbers/numbers_helpers'
-
 RSpec.describe Object, '#predicate_matchers' do
   let(:empty_string) { String.new }
-  # let(:odd_number) { Number::Odd.give_me_odd_number }
+  let(:odd_number) { 3 }
   let(:even_number) { 2 }
 
-  xit '3 is odd?' do
+  it '3 is odd?' do
     expect(odd_number).to be_odd
   end
 
