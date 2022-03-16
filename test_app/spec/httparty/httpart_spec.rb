4d616001a7789e903749db166986e881fe9f4010
thiago1bd2 <thiagodaqui@gmail.com>
2022-03-16 15:02:28 -0300

    uso do vcr

--- test_app/spec/httparty/httpart_spec.rb
+++ test_app/spec/httparty/httpart_spec.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe HTTParty do
-  it '#get' do
+  it '#get', vcr: { cassette_name: 'jsonplaceholder/posts' } do
     # stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2')
     #   .to_return(status: 200, body: '',
     #              headers: {
     #                'Content-Type' => 'application/json; charset: utf-8'
     #              })
-
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.header['content-type']
-    expect(content_type).to match(%r{application/json})
+    expect(content_type).to match 'application/json'
   end
 end
