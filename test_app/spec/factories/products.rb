4af43f084d9ada12c6accab16a30f3424055103a
thiago1bd2 <thiagodaqui@gmail.com>
2022-04-21 14:06:56 -0300

    rebuilding the app from scratch

--- test_app/spec/factories/products.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :product do
-    description { Faker::Commerce.product_name }
-    price { Faker::Commerce.price }
-    category
-  end
-end
